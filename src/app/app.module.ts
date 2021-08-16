413eff2cf3e629b2416551d9d99ac9f0ee4eb340
Sergio Gordillo <sergiogordillodev@gmail.com>
2021-08-16 15:05:13 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from './material/material/material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
