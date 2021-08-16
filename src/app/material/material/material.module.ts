e5725b637bc1b86fd928a1a5ec245f8ccea186a8
Sergio Gordillo <sergiogordillodev@gmail.com>
2021-08-16 21:05:01 +0200

    Playing with topbar menu, it already works

--- src/app/material/material/material.module.ts
+++ src/app/material/material/material.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
@@ -11,7 +12,10 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     CommonModule,
   ],
   exports:[
+
+    MatButtonModule,
     MatIconModule,
+ 
     MatToolbarModule
   ]
 })
